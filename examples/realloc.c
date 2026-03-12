020cbde2415606b1bbd99740cb165e4ef4c629b3
JGP-1208 <josgp1208@gmail.com>
2026-03-12 14:01:36 -0600

    Add dynamic memory allocation for integer arrays
    Implement memory allocation and reallocation for integer arrays.

--- examples/realloc.c
+++ examples/realloc.c
@@ -2,29 +2,32 @@
 #include <stdlib.h>
 
 
-int main (){
-  // TODO(1): Declara dos apuntadores a entero llamados array, array2
-  // TODO(2): Declara una variable para indicar cuantos elementos se requieren
-  
-  printf("¿Cuantos elementos tiene el conjunto?\n");
-  // TODO(3): Lee el numero de elementos
-  // TODO(4): Usando una llamada a malloc reserva la memoria para array
-  
-  if (array!=NULL) {
-    // TODO(5): Usando un ciclo for inserta los datos en el arreglo
-    printf("[ ");
-    // TODO(6): Usando un ciclo for imprime la información en el arreglo
-    printf("]\n");
 
-    // TODO(7): Asigna a array2, el arreglo duplicando su tamaño usando realloc
-     if (array2!=NULL) {
-        // TODO(8): Usando un ciclo for inserta los datos faltantes en el arreglo
-        printf("[ ");
-        // TODO(9): Usando un ciclo for imprime la información en el arreglo
-        printf("]\n");
-      }
-    
-    // TODO(10): libera la memoria reservada por realloc
-  }
-  return 0;
+
+int main (){
+ int *array, *array2;
+ int num;
+  printf("¿Cuantos elementos tiene el conjunto?\n");
+ scanf("%d", &num);
+ array = (int*) malloc(sizeof(int)*num);
+  if (array!=NULL) {
+   printf("[ ");
+   for(int i=0; i < num; i++)
+   printf("%d ", *(array + i));
+   printf("]\n");
+
+
+   array2 = (int*) realloc(array, (2*num)*sizeof(int));
+    if (array2!=NULL) {
+       for(int i=0; i < num; i++)
+       scanf("%d ", *(array2 + i));
+       printf("[ ");
+       for(int i=0; i < num; i++)
+       printf("%d ", *(array2 + i));
+       printf("]\n");
+     }
+  
+   free(array2);
+ }
+ return 0;
 }
